4bf723538e98e912e29d41a9c41eccfdb9eeb2a4
raghid osseiran <Raghidcrazy4@hotmail.com>
2023-12-11 20:55:44 +0100

    fully functionning hamburger menu using js script to modify the css properties of the class, now adaptable to mobile navigation

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    /* when clicked will add or remove the open class in that elements */
+}
